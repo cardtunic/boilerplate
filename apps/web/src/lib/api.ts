a99feddee4306a2c77cb8ba71512e53c8faab656
kaleb <cardtunic@gmail.com>
2026-08-16 11:26:35 -0300

    chore: configure oxlint and oxfmt

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -4,11 +4,9 @@ import type { AppType } from "api/rpc";
 export function api(platform?: Readonly<App.Platform> | undefined) {
   if (!import.meta.env.SSR) return hc<AppType>(window.location.origin).api;
 
-  if (!platform)
-    throw new Error("Can't created API on server without platform.");
+  if (!platform) throw new Error("Can't created API on server without platform.");
 
   return hc<AppType>("https://api.internal", {
-    fetch: (input: URL | RequestInfo, init?: RequestInit) =>
-      platform.env.API.fetch(input, init),
+    fetch: (input: URL | RequestInfo, init?: RequestInit) => platform.env.API.fetch(input, init),
   }).api;
 }
