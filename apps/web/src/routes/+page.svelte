7eee83057419e76dedebbef1abd9c104927749c2
kaleb <cardtunic@gmail.com>
2026-08-28 14:18:54 -0300

    feat(web/homepage): revamp homepage

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<a href="/login"><button>Login</button></a>
+<script lang="ts">
+  import Button from "$lib/components/ui/button/button.svelte";
+  import { CuboidIcon, UserIcon } from "@lucide/svelte";
+</script>
+
+<main class="w-screen h-screen flex flex-col gap-6 justify-center items-center">
+  <CuboidIcon class="size-8 text-primary" />
+
+  <p class="max-w-lg text-center leading-6">
+    Simple SvelteKit + Hono and EffectTS boilerplate.
+  </p>
+
+  <a href="/register">
+    <Button size="lg" class="w-full">Create a test account <UserIcon /></Button>
+  </a>
+</main>
